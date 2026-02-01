3fc608e85a16e6ff1a0fdd5157bad40b7a656ac9
Yasser Agha <yasseragha.dev@gmail.com>
2026-02-01 20:51:34 +0300

    build Home Cards

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,27 @@
 import {MdBorderColor} from "react-icons/md";
-// import {BsSunglasses} from "react-icons/bs";
-import Image from 'next/image'
+import {BsSunglasses} from "react-icons/bs";
+import Link from "next/link";
 
 
 export default function Home() {
+
+    const cardItems = [
+        {
+            icon: MdBorderColor,
+            text: "Orders",
+            to: "/orders",
+        },
+        {
+            icon: BsSunglasses,
+            text: "Products",
+            to: "/products",
+        },
+        {
+            icon: MdBorderColor,
+            text: "Customer",
+            to: "/customers",
+        },
+    ]
     return (<>
             <div className={"container  "}>
                 <div className={"title flex items-center space-x-2 mb-2 "}>
@@ -13,14 +31,31 @@ export default function Home() {
 
 
                 <div
-                    className={"flex items-center justify-between w-full bg-stone-700 rounded-lg overflow-hidden p-2 "}>
-                    <div
-                        className={"card w-50 h-50 rounded-md bg-emerald-600  p-4 flex flex-col justify-center items-center shadow-lg"}>
-                        <Image src={"/images/lab.jpg"} alt={"logo"} width={120} height={20}
-                               className={"rounded-full"}></Image>
+                    className="grid  grid-cols-2 sm:grid-cols-3 md:grid-cols-4  gap-6  mx-auto bg-zinc-800 overflow-hidden  rounded-lg px-6 py-4 "
+                >
 
 
-                    </div>
+                    {cardItems.map((item, index) => {
+                        const Icon = item.icon;
+
+                        return (
+                            <Link
+                                href={item.to}
+                                key={index}
+                                className="card w-full py-6 px-10 hover:-translate-y-2 transition duration-300 rounded-xl bg-zinc-500 flex flex-col justify-center items-center shadow-lg"
+                            >
+                                <div className="card-body bg-white text-7xl p-4 rounded-3xl text-black mb-4">
+                                    <Icon />
+                                </div>
+
+                                <p className="text-lg font-bold line-clamp-2">
+                                    {item.text}
+                                </p>
+                            </Link>
+                        );
+                    })}
+
+
 
                 </div>
 
