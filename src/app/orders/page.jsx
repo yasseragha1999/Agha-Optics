9264486d81b93694869580cce3eea83792189ad1
Yasser Agha <yasseragha.dev@gmail.com>
2026-02-02 18:07:59 +0300

    refactor Tabs in Orders

--- src/app/orders/page.jsx
+++ src/app/orders/page.jsx
@@ -6,37 +6,38 @@ const Orders = () => {
 
     return (
         <section className="container">
-            <div className="flex justify-between mb-3">
-                <h3 className="text-3xl font-bold">Create Order</h3>
+            <div className="mb-4">
+                <h3 className="text-3xl font-bold mb-2">Create Order</h3>
+                <div className="flex justify-center">
+                    <div
+                        className="flex justify-center  bg-slate-900 p-1 border border-black rounded-full text-sm">
 
-                <div
-                    className="md:flex justify-center w-fit space-x-2 bg-slate-900 p-1 border border-black rounded-2xl md:rounded-full text-sm">
+                        {["prescription", "frame", "lens"].map((item) => (
+                            <div key={item} className="flex items-center">
+                                <input
+                                    type="radio"
+                                    name="options"
+                                    id={item}
+                                    className="hidden peer"
+                                    checked={checked === item}
+                                    onChange={() => setChecked(item)}
+                                />
 
-                    {["prescription", "frame", "lens"].map((item) => (
-                        <div key={item} className="flex items-center">
-                            <input
-                                type="radio"
-                                name="options"
-                                id={item}
-                                className="hidden peer"
-                                checked={checked === item}
-                                onChange={() => setChecked(item)}
-                            />
-
-                            <label
-                                htmlFor={item}
-                                className="cursor-pointer rounded-full py-2 px-9 text-gray-500 transition-colors duration-200
+                                <label
+                                    htmlFor={item}
+                                    className="cursor-pointer rounded-full py-2 px-9 text-gray-500 transition-colors duration-200
                 peer-checked:bg-indigo-600 peer-checked:text-white"
-                            >
-                                {item.toUpperCase()}
-                            </label>
-                        </div>
-                    ))}
+                                >
+                                    {item.toUpperCase()}
+                                </label>
+                            </div>
+                        ))}
+                    </div>
                 </div>
-            </div>
 
+            </div>
             {/* Content */}
-            <div >
+            <div>
                 {checked === "prescription" && <p>HTML Content</p>}
                 {checked === "frame" && <p>CSS Content</p>}
                 {checked === "lens" && <p>React Content</p>}
