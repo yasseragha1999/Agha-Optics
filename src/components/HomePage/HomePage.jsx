f7676c76231f37583ef17b81cb703c65facac828
Yasser Agha <yasseragha.dev@gmail.com>
2026-02-13 18:52:27 +0300

    build curd Home Page

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
+import {IoCreate} from "react-icons/io5";
 
 const HomePage = () => {
     return (
-        <>
-            <h3 className="text-transparent bg-clip-text bg-gradient-to-br from-green-500 to-blue-500">
-                Agha Control
-            </h3>
+        <div className={"grid grid-cols-1 md:grid-cols-2 gap-8  shadow-2xl rounded-lg w-full  "}>
 
-        </>
+            <div className={" bg-linear-to-r  from-slate-700 to-slate-500 rounded-xl flex flex-col gap-4 px-4 py-2"}>
+                <IoCreate size={80}/>
+                <h6 className={"text-2xl font-bold"}>Create Order</h6>
+                <p className={"text-lg "}>Add a new customer order</p>
+            </div>
+            <div className={" bg-linear-to-r from-slate-700 to-slate-500 rounded-xl flex flex-col gap-4 px-4 py-2"}>
+                <IoCreate size={80}/>
+                <h6 className={"text-2xl font-bold"}>Manage Frames</h6>
+                <p className={"text-lg "}>Add / Edit Frames</p>
+                <p className={"text-lg "}>stock</p>
+            </div>
+            <div className={" bg-linear-to-r from-slate-700 to-slate-500 rounded-xl flex flex-col gap-4 px-4 py-2"}>
+                <IoCreate size={80}/>
+                <h6 className={"text-2xl font-bold"}>Manage Lenses</h6>
+                <p className={"text-lg "}>Add Lens Types</p>
+                <p className={"text-lg "}>Pricing</p>
+            </div>
+            <div className={" bg-linear-to-r from-slate-700 to-slate-500 rounded-xl flex flex-col gap-4 px-4 py-2"}>
+                <IoCreate size={80}/>
+                <h6 className={"text-2xl font-bold"}>View Orders</h6>
+                <p className={"text-lg "}>See Previous Orders</p>
+                <p className={"text-lg "}>Print Again</p>
+            </div>
+
+
+        </div>
+
+
     );
 };
 
