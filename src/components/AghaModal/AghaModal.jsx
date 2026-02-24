c077791710b5d5b7b8628d19c5e161ae33f2f558
Yasser Agha <yasseragha.dev@gmail.com>
2026-02-24 23:51:48 +0300

    build modal in center

--- src/components/AghaModal/AghaModal.jsx
+++ src/components/AghaModal/AghaModal.jsx
@@ -1,19 +1,19 @@
 const AghaModal = ({closeModal}) => {
     return (
-        <div>
-            <form className="bg-white text-gray-500 max-w-96 mx-4 md:p-6 p-4 text-left text-sm rounded-lg shadow-lg">
-                <h2 className="text-2xl font-semibold mb-6 text-center text-gray-800">Need Help? Contact Us</h2>
-                <label htmlFor="email">Email</label>
-                <input id="email" className="w-full border mt-1 border-gray-500/30 outline-none rounded p-2"
-                       type="email" placeholder="Enter your email" required/>
-                <textarea rows="4" id="msg"
-                          className="w-full border mt-3 resize-none border-gray-500/30 outline-none rounded p-2"
-                          placeholder="Your Message..." required></textarea>
-                <button onClick={closeModal} type="submit"
-                        className="w-full my-3 bg-indigo-500 hover:bg-indigo-600/90 active:scale-95 transition py-2.5 rounded text-white">close
-                </button>
+        <>
+            <form
+                className={'bg-white fixed inset-1/4 z-10  shadow-lg text-gray-500 rounded-lg px-6 py-4'}>
+                <div>
+                    <div className='flex items-center gap-2'>
+                        <svg className="w-6 h-6 text-black" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" >
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 10h16M8 14h8m-4-7V4M7 7V4m10 3V4M5 20h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z" />
+                        </svg>
+                        <label htmlFor="destinationInput">Destination</label>
+                    </div>
+                    <input list='destinations' id="destinationInput" type="text" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none" placeholder="Type here" required />
+                </div>
             </form>
-        </div>
+        </>
     );
 };
 
