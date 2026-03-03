33aa7f7033cf3f5ee1d763aed6c088f214bc3843
Yasser Agha <yasseragha.dev@gmail.com>
2026-03-03 10:44:01 +0300

    change width content

--- src/components/AghaModal/AghaModal.jsx
+++ src/components/AghaModal/AghaModal.jsx
@@ -65,7 +65,7 @@ const AghaModal = ({closeModal, onSave}) => {
                         <button
                             type="button"
                             onClick={closeModal}
-                            className="px-4 py-2 max-w-fit bg-red-400 rounded hover:bg-red-500 cursor-pointer"
+                            className="px-4 py-2 min-w-fit bg-red-400 rounded hover:bg-red-500 cursor-pointer"
                         >
                             Cancel
                         </button>
