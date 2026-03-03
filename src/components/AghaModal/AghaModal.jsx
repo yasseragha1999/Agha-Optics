c0bb3ae653a94a0e40ac989604ac45ff62e7961f
Yasser Agha <yasseragha.dev@gmail.com>
2026-03-03 10:40:13 +0300

    change width content

--- src/components/AghaModal/AghaModal.jsx
+++ src/components/AghaModal/AghaModal.jsx
@@ -15,6 +15,7 @@ const AghaModal = ({closeModal, onSave}) => {
 
         onSave({name, price});
         closeModal();
+        console.log(name, price);
     };
     return (
         <>
@@ -36,8 +37,7 @@ const AghaModal = ({closeModal, onSave}) => {
                             type="text"
                             value={name}
                             onChange={(e) => setName(e.target.value)}
-                            className="rounded border border-gray-200 px-3 py-2 mt-2 w-full outline outline-gray-300 focus:outline-blue-400"
-                            placeholder="Frame name..."
+                            className="rounded border border-gray-200 px-3 py-2 mt-2  w-full outline outline-gray-300 focus:outline-blue-400"
                             required
                         />
                     </div>
@@ -55,8 +55,7 @@ const AghaModal = ({closeModal, onSave}) => {
                             type="number"
                             value={price}
                             onChange={(e) => setPrice(e.target.value)}
-                            className="rounded border border-gray-200 px-3 py-2 mt-2 w-full outline outline-gray-300 focus:outline-blue-400"
-                            placeholder="Price..."
+                            className="rounded border border-gray-200 text-black  px-3 py-2 mt-2 w-full outline outline-gray-300 focus:outline-blue-400"
                             required
                         />
                     </div>
@@ -66,14 +65,14 @@ const AghaModal = ({closeModal, onSave}) => {
                         <button
                             type="button"
                             onClick={closeModal}
-                            className="px-4 py-2 bg-red-500 rounded hover:bg-red-600"
+                            className="px-4 py-2 max-w-fit bg-red-400 rounded hover:bg-red-500 cursor-pointer"
                         >
                             Cancel
                         </button>
 
                         <button
                             type="submit"
-                            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+                            className="px-4 py-2 max-w-fit bg-blue-500 text-white rounded hover:bg-blue-600 cursor-pointer"
                         >
                             Save
                         </button>
